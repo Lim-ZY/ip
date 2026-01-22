eeedabe71dde4817d8ea31002158d13b5a7b1afc
Lim-ZY <tcmpiano03@gmail.com>
2026-01-22 13:58:19 +0800

    Add completion state to task

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,12 +1,23 @@
 public class Task {
     private final String name;
+    private boolean done;
 
     public Task(String name) {
         this.name = name;
+        this.done = false;
+    }
+
+    public void markDone() {
+        this.done = true;
+    }
+
+    public void markUndone() {
+        this.done = false;
     }
 
     @Override
     public String toString() {
-        return this.name;
+        String doneState = this.done ? "X" : " ";
+        return String.valueOf("[" + doneState + "] " + this.name);
     }
 }
--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -12,6 +12,31 @@ public class TaskList {
         tasks.add(t);
     }
 
+    public boolean markDone(int id) {
+        if (id >= tasks.size()) {
+            System.out.println("ID not defined. Please try again.");
+            return false;
+        }
+        this.tasks.get(id).markDone();
+        return true;
+    }
+
+    public boolean markUndone(int id) {
+        if (id >= tasks.size()) {
+            System.out.println("ID not defined. Please try again.");
+            return false;
+        }
+        this.tasks.get(id).markUndone();
+        return true;
+    }
+
+    public void printTask(int id) {
+        if (id >= tasks.size()) {
+            System.out.println("ID not defined. Please try again.");
+        }
+        System.out.println(this.tasks.get(id).toString());
+    }
+
     public void printTasks() {
         int i = 1;
         for (Task t : this.tasks) {
