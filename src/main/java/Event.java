f03313326afe1017532ed6445e35cb675fe78591
Lim-ZY <tcmpiano03@gmail.com>
2026-01-22 15:21:10 +0800

    Add task subclasses

---
+++ src/main/java/Event.java
@@ -0,0 +1,15 @@
+public class Event extends Task {
+    String fromDate;
+    String toDate;
+
+    public Event(String name, String from, String to) {
+        super(name);
+        this.fromDate = from;
+        this.toDate = to;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (from: " + this.fromDate + " to: " + this.toDate + ")";
+    }
+}
