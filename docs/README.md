63cb72519e3e986e52573d2ded503c6f4007d72f
Lim-ZY <tcmpiano03@gmail.com>
2026-01-22 09:51:20 +0800

    Add greeting and exit

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Mark User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Mark.java
@@ -0,0 +1,12 @@
+public class Mark {
+    public static void main(String[] args) {
+        String logo = "  __  __            _    \n" +
+                " |  \\/  |          | |   \n" +
+                " | \\  / | __ _ _ __| | __\n" +
+                " | |\\/| |/ _` | '__| |/ /\n" +
+                " | |  | | (_| | |  |   < \n" +
+                " |_|  |_|\\__,_|_|  |_|\\_\\\n";
+        System.out.println("Hello! I'm \n" + logo + "What can I do for you?\n");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
