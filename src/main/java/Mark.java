c3b04c952f37b15cc9c7d8d3dd9fb5eb474dea47
Lim-ZY <tcmpiano03@gmail.com>
2026-01-29 18:50:34 +0800

    Refactor Mark command parsing code
    Without polymorphism, the code was unnecessarily complex as parsing was
    done in Mark.java.
    
    Abstracting Command and using Parser allows us to reduce the code to
    its intended logic.

--- src/main/java/Mark.java
+++ src/main/java/Mark.java
@@ -1,12 +1,10 @@
 import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 
 public class Mark {
     private boolean running;
     private TaskList tasks;
     private Ui ui;
+    private Storage storage;
 
     public Mark() {
         this.running = true;
@@ -15,17 +13,13 @@ public class Mark {
 
     public static void main(String[] args) {
         Mark mark = new Mark();
-        try {
-            mark.run();
-        } catch (IOException e) {
-            Ui.println(e.getMessage());
-        }
+        mark.run();
     }
 
-    private void run() throws IOException {
-        Storage storage = new Storage("./data/data.txt");
+    private void run() {
+        this.storage = new Storage("./data/data.txt");
         try {
-            this.tasks = new TaskList(storage.getFileContents());
+            this.tasks = new TaskList(this.storage.getFileContents());
         } catch (IOException e) {
             ui.showLoadingError();
             this.tasks = new TaskList();
@@ -35,106 +29,12 @@ public class Mark {
         
         while (this.running) {
             try {
-                String[] input = ui.readInput();
-                Ui.printDivider();
-
-                switch (input[0]) {
-                case "list": {
-                    this.tasks.printTasks();
-                    continue;
-                }
-                case "bye": {
-                    this.bye(storage);
-                    this.running = false;
-                    continue;
-                }
-                case "mark": {
-                    try {
-                        int id = Integer.parseInt(input[1]) - 1;
-                        this.tasks.markDone(id);
-                    } catch (MarkException e) {
-                        Ui.printException(e);
-                    }
-                    continue;
-                }
-                case "unmark": {
-                    try {
-                        int id = Integer.parseInt(input[1]) - 1;
-                        this.tasks.markUndone(id);
-                    } catch (MarkException e) {
-                        Ui.printException(e);
-                    }
-                    continue;
-                }
-                case "delete": {
-                    try {
-                        int id = Integer.parseInt(input[1]) - 1;
-                        this.tasks.delete(id);
-                    } catch (MarkException e) {
-                        Ui.printException(e);
-                    }
-                    continue;
-                }
-                case "todo": {
-                    if (input[1].isEmpty()) {
-                        Ui.println("Oh no... the description of a todo cannot be empty. Please try again.");
-                        Ui.printDivider();
-                    } else {
-                        this.tasks.addTask(new Todo(input[1]));
-                    }
-                    break;
-                }
-                case "deadline": {
-                    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
-                    if (!input[1].contains("/by")) {
-                        throw new MarkException("usage: deadline <task> /by <YYYY-MM-DD> <HHMM>");
-                    }
-                    String deadline = input[1].substring(input[1].indexOf("/by") + 4);
-                    String taskName = input[1].substring(0, input[1].indexOf("/by")).trim();
-                    LocalDateTime date;
-                    try {
-                        date = LocalDateTime.parse(deadline, dtf);
-                    } catch (DateTimeParseException e) {
-                        throw new MarkException("usage: deadline <task> /by <YYYY-MM-DD> <HHMM>");
-                    }
-                    this.tasks.addTask(new Deadline(taskName, date));
-                    break;
-                }
-                case "event": {
-                    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
-                    if (!input[1].contains("/from") || !input[1].contains("/to")) {
-                        throw new MarkException("usage: event <task> /from <YYYY-MM-DD> <HHMM> "
-                                + "/to <YYYY-MM-DD> <HHMM>");
-                    }
-                    LocalDateTime fromDate, toDate;
-                    try {
-                        fromDate = LocalDateTime.parse(input[1].substring(input[1].indexOf("/from") + 6,
-                                input[1].indexOf("/to")).trim(), dtf);
-                        toDate = LocalDateTime.parse(input[1].substring(input[1].indexOf("/to") + 4), dtf);
-                    } catch (DateTimeParseException e) {
-                        throw new MarkException("usage: event <task> /from <YYYY-MM-DD> <HHMM> " 
-                                + "/to <YYYY-MM-DD> <HHMM>");
-                    }
-                    String taskName = input[1].substring(0, input[1].indexOf("/from")).trim();
-                    this.tasks.addTask(new Event(taskName, fromDate, toDate));
-                    break;
-                }
-                default: {
-                    throw new MarkException("I'm sorry, I don't know what that means :( Please try again.");
-                }}
-                
-                Ui.print("Got it. I've added this task:\n\t");
-                this.tasks.printTask(this.tasks.length() - 1);
-                Ui.println("Now you have " + this.tasks.length() + " tasks in the list.");
-                Ui.printDivider();
-            } catch (MarkException e) {
+                Command c = Parser.parse(ui.readInput());
+                c.execute(this.tasks, this.ui, this.storage);
+                this.running = !c.isExit();
+            } catch (InvalidFormatException | IOException e) {
                 Ui.printException(e);
             }
         }
-    }
-
-    private void bye(Storage storage) {
-        this.tasks.saveTasks(storage);
-        ui.bye();
     }
 }
---
+++ src/main/java/Parser.java
@@ -0,0 +1,66 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class Parser {
+    private static DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
+    
+    public static Command parse(String input) throws InvalidFormatException {
+        String[] segments = input.trim().split(" ", 2);
+        String action = segments[0];
+        
+        switch (action) {
+        case "list": 
+            return new ListCommand();
+        case "bye":
+            return new ByeCommand();
+        case "mark":
+            return new MarkCommand(Integer.parseInt(segments[1]) - 1);
+        case "unmark":
+            return new UnmarkCommand(Integer.parseInt(segments[1]) - 1);
+        case "delete":
+            return new DeleteCommand(Integer.parseInt(segments[1]) - 1);
+        case "todo":
+            return new TodoCommand(segments);
+        case "deadline":
+            return Parser.parseDeadline(segments);
+        case "event":
+            return Parser.parseEvent(segments);
+        default:
+            return new UnknownCommand();
+        }
+    }
+    
+    public static Command parseDeadline(String[] segments) throws InvalidFormatException {
+        if (!segments[1].contains("/by")) {
+            throw new InvalidFormatException("Usage: deadline <task> /by <YYYY-MM-DD> <HHMM>");
+        }
+        String deadline = segments[1].substring(segments[1].indexOf("/by") + 4);
+        String taskName = segments[1].substring(0, segments[1].indexOf("/by")).trim();
+        LocalDateTime date;
+        try {
+            date = LocalDateTime.parse(deadline, dtf);
+        } catch (DateTimeParseException e) {
+            throw new InvalidFormatException("Usage: deadline <task> /by <YYYY-MM-DD> <HHMM>");
+        }
+        return new DeadlineCommand(taskName, date);
+    }
+    
+    public static Command parseEvent(String[] segments) throws InvalidFormatException {
+        if (!segments[1].contains("/from") || !segments[1].contains("/to")) {
+            throw new InvalidFormatException("usage: event <task> /from <YYYY-MM-DD> <HHMM> "
+                    + "/to <YYYY-MM-DD> <HHMM>");
+        }
+        LocalDateTime fromDate, toDate;
+        try {
+            fromDate = LocalDateTime.parse(segments[1].substring(segments[1].indexOf("/from") + 6,
+                    segments[1].indexOf("/to")).trim(), dtf);
+            toDate = LocalDateTime.parse(segments[1].substring(segments[1].indexOf("/to") + 4), dtf);
+        } catch (DateTimeParseException e) {
+            throw new InvalidFormatException("usage: event <task> /from <YYYY-MM-DD> <HHMM> "
+                    + "/to <YYYY-MM-DD> <HHMM>");
+        }
+        String taskName = segments[1].substring(0, segments[1].indexOf("/from")).trim();
+        return new EventCommand(taskName, fromDate, toDate);
+    }
+}
